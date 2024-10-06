ddf8728e662b51e5b6605f2687fb8d99026a0e4e
Carlos Eduardo Fiori dos Santos <carlos.fiori@hotmail.com>
2024-10-06 17:38:15 -0300

    feat: add new form component and improve the forms

--- app/main/(tabs)/collection.tsx
+++ app/main/(tabs)/collection.tsx
@@ -1,57 +1,7 @@
-import StyledConfirmation from "@/components/styled-confirmation";
-import StyledInput from "@/components/styled-input";
-import { useCatalogDatabase } from "@/database/useCatalogDatabase";
-import { CatalogProps } from "@/interfaces";
-import { useState } from "react";
-import { StyleSheet, ScrollView, View, ToastAndroid } from "react-native";
+import CollectionForm from "@/app/main/content/collection";
 
 export default function Collection() {
-    const [collection, setCollection] = useState<CatalogProps>({} as CatalogProps);
-    const catalogDatabase = useCatalogDatabase();
-
-    function handleCreate() {
-        if (!collection.name) return ToastAndroid.showWithGravity('Preencha os campos obrigatórios', ToastAndroid.SHORT, ToastAndroid.TOP);
-
-        catalogDatabase.create(collection).then(() => {
-            ToastAndroid.showWithGravity('Criado com sucesso!', ToastAndroid.SHORT, ToastAndroid.TOP);
-        }).catch((error) => {
-            ToastAndroid.showWithGravity('Erro ao criar' + error, ToastAndroid.SHORT, ToastAndroid.TOP);
-        })
-    }
-
     return (
-        <ScrollView contentContainerStyle={style.container}>
-            <View style={style.containerInput}>
-                <StyledInput label="Nome da especie" type="text" placeholder="Digite o nome da especie" onChangeText={(value) => { setCollection({ ...collection, name: value }) }} />
-                <StyledInput label="Tempo de vida" type="text" placeholder="Digite o tempo de vida" onChangeText={(value) => { setCollection({ ...collection, lifeTime: value }) }} />
-                <StyledInput label="Época de plantio" type="text" placeholder="Digite a época de plantio" onChangeText={(value) => { setCollection({ ...collection, plantTime: value }) }} />
-                <StyledInput label="Cultivo" type="text-area" placeholder="Digite sobre o cultivo" onChangeText={(value) => { setCollection({ ...collection, cultivation: value }) }} />
-                <StyledInput label="Atenção" type="text-area" placeholder="Digite sobre as precauções e observações" onChangeText={(value) => { setCollection({ ...collection, warning: value }) }} />
-
-                <View style={style.containerButtons}>
-                    <StyledConfirmation firClick={() => { setCollection({} as CatalogProps) }} firLabel="Resetar" secClick={handleCreate} secLabel="Criar" />
-                </View>
-            </View>
-        </ScrollView>
+        <CollectionForm />
     )
 }
-
-const style = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        flex: 1
-    },
-    containerInput: {
-        width: '100%',
-        padding: 20,
-        marginTop: 20,
-        flex: 1,
-        rowGap: 20,
-    },
-    containerButtons: {
-        marginTop: 40
-    }
-})
